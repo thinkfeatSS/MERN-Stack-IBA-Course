1e4edff655aa36428e2d1056db523535c6167289
ismailsindhi <thinkfeat@gmail.com>
2023-11-06 18:06:08 +0500

    js first class

---
+++ JS/1.js
@@ -0,0 +1,40 @@
+// "use strict";
+
+// Convetion for variables
+// always use small for first character and camel case
+
+// var firtName = "Jatendar";
+// console.log(firtName);
+
+// firtName = "Rizwan";
+// console.log(firtName);
+
+// valueOne = 10;
+// valueTwo = 50;
+// console.log(valueOne+valueTwo);
+
+// let clases = "MERN Stack";
+// const pi = 3.14
+// clases = "Andrioid Class";
+// console.log(clases);
+
+// pi = 2.5;
+// console.log(pi);
+
+// ____________________________________________________________
+
+// Operators
+
+let valueOne = 5
+let valueTwo = 15
+
+let addition = valueOne + valueTwo
+console.log(addition)
+
+let firstName = "Imtaiz";
+let lastName = "Luhur";
+let fullName = firstName+lastName
+console.log(fullName)
+
+console.log("sum of "+valueOne+" and "+valueTwo+" is = "+addition)
+console.log(`Sum of ${valueOne} and ${valueTwo} is = ${addition}`)
