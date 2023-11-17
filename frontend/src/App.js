1e35b0d9bfbe666dccbc6ea58e91fb88dd37dafd
ismailsindhi <thinkfeat@gmail.com>
2023-11-18 03:00:52 +0500

    project update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
+import Hero from "./components/Hero";
 import Home from "./pages/Home"
+
 function App() {
   return (
     <div className="App">
       <Home />
-     
+      <Hero />
+      <Hero />
+
     </div>
   );
 }
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,30 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-import PrimaryButton from "./PrimaryButton";
-import SecondaryButton from "./SecondaryButton";
 import LineIcon from "./LineIcon";
 function NavBar() {
     const [isOpen, setisOpen] = useState(false)
+    const [sticky, setSticky] = useState(false)
+
+    useEffect(()=>{},[])
   const menuLinks = ["Home", "About", "Skills", "Projects", "Contact"];
 
   return (
     <>
-    <nav className="flex justify-between items-center">
+    <nav className="fixed w-full top-0 left-0">
+      <div className="flex justify-between items-center ">
+
       <h1>Name</h1>
-      <div className="hidden md:block">
-        <ul className="flex text-lg gap-4">
+      <div className="hidden md:block ">
+        <ul className="flex text-lg gap-4 bg-white text-gray-900 py-2 px-6 rounded-bl-full">
           {menuLinks.map((item) => {
-              return <li>{item}</li>;
-            })}
+            return <li>{item}</li>;
+          })}
         </ul>
       </div>
-      <div className="md:hidden" onClick={()=>{setisOpen(!isOpen)}}>
+      </div>
+      <div className={`${isOpen ? 'text-gray-900' : 'right-[-100%]'} z-10 md:hidden`} onClick={()=>{setisOpen(!isOpen)}}>
         <LineIcon />
       </div>
       
-    </nav>
+    </nav>  
     <div className=
-    {`${isOpen ? 'right-0' : 'right-[-100%]'} absolute h-full w-3/4 bg-slate-200 duration-300 md:hidden `}>
+    {`${isOpen ? 'right-0' : 'right-[-100%]'} top-0 absolute h-full w-3/4 bg-slate-200 duration-300 md:hidden `}>
     <ul className="flex flex-col items-center text-lg gap-4 text-gray-900 p-6">
           {menuLinks.map((item) => {
               return <li className="hover:text-blue-700" onClick={()=>{setisOpen(!isOpen)}}>{item}</li>;
